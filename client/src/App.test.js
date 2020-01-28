6027823442da73f645515d8ee3a53050d1b39701
bret-knight87 <bret.knight87@gmail.com>
2020-01-27 21:30:41 -0600

    tests passed using yarn instead of npm

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import * as rtl from '@testing-library/react'
 import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
 
 import NavBar from './components/NavBar';
 
-test('NavBar renders without crashing', () => {
+it('NavBar renders without crashing', () => {
   const { getByText } = render(<NavBar />)
   getByText("Soccer Cards")
 })
 
-test('clicking the toggle button actually changes class to dark mode', () => {
+it('clicking the toggle button actually changes class to dark mode', () => {
   const { getByTestId } = rtl.render(<NavBar />)
   const darkToggleButton = getByTestId('darkToggleButton')
   expect(darkToggleButton.classList.contains('toggled')).toBe(false)
